59cff1530f4899cc2170c7bf9e602aaf9b65038c
zzz <zzz@z.com>
2019-08-12 04:59:28 +0300

    wip

--- SoftReferenceExample.java
+++ SoftReferenceExample.java
@@ -1,42 +1,45 @@
 import java.lang.ref.SoftReference;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class SoftReferenceExample {
 
+    static volatile boolean finalized = false;
+
+    static class LargeObject {
+        final byte[] data = new byte[20_000_000];
+
+        @Override
+        protected void finalize() throws Throwable {
+            finalized = true;
+            System.out.println("finalize()... finalized set to " + finalized);
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        final int WEAK_SIZE = 20_000_000;
-        final int LEAK_INCREMENT = 9_000_000;
-
-        Map<String, SoftReference<byte[]>> imageCache = new HashMap<>();
-        byte[] image = new byte[20_000_000];
-        imageCache.put("1.jpg", new SoftReference<>(image));
-        image = null;
-
-        //WeakReference<byte[]> weak = new WeakReference<>(new byte[WEAK_SIZE]);
-        SoftReference<byte[]> soft = new SoftReference<>(new byte[WEAK_SIZE]);
+        var soft1 = new SoftReference<>(new LargeObject());
 
         System.out.println("forcing OOM... wait");
         int i = 0;
         try {
             List<byte[]> memLeak = new ArrayList<>();
             for (; ; i++) {
-                SoftReference<byte[]> softRef = imageCache.get("1.jpg");
-                if (softRef.get() == null) {
-                    System.out.println("SUCCESS: softRef: " + softRef.get() + " at " + i);
+                if (soft1.get() == null) {
+                    System.out.println();
+                    System.out.println("SUCCESS: softRef: " + soft1.get() + " at " + i);
                 }
-                memLeak.add(new byte[LEAK_INCREMENT]);
-                if (i > 0 && i % 500 == 0) {
+                memLeak.add(new byte[10_000_000]);
+                if (i > 0 && i % 10 == 0) {
+                    System.out.print(".");
                     System.gc();
                 }
                 TimeUnit.MILLISECONDS.sleep(3);
             }
         } catch (OutOfMemoryError e) {
-            System.out.println("OOM at " + i);
+            System.out.println();
+            System.out.println("OOM at " + i + ", finalized? " + finalized);
+            System.out.println(finalized ? "PASSED" : "FAIL");
         }
     }
 }
