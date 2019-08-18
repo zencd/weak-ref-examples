4c61c492fa1b2cf4a5dcbcda6c4fcd2536ca6bd9
zzz <zzz@z.com>
2019-08-19 00:15:06 +0300

    wip

--- WeakHashMapExample.java
+++ WeakHashMapExample.java
@@ -5,13 +5,13 @@ import java.util.concurrent.TimeUnit;
 class WeakHashMapExample {
     public static void main(String[] args) throws InterruptedException {
         Map<Object, Object> map = new WeakHashMap<>();
-        //Map<Data, String> map = new HashMap<>();
         map.put(new Object(), "dummy");
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("map size: " + map.size() + " ... gc");
-            System.gc();
-            TimeUnit.MILLISECONDS.sleep(1);
-        }
+        System.out.println("map size: " + map.size());
+        System.out.println("gc");
+        System.gc();
+        TimeUnit.MILLISECONDS.sleep(500);
+        System.out.println("map size: " + map.size());
+        System.out.println(map.size() == 0 ? "SUCCESS" : "FAIL");
     }
 }
