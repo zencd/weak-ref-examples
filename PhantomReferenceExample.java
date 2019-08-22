f613305fd20fcc924bdf5a6ee2fdc5e1beaa13ef
zzz <zzz@z.com>
2019-08-22 05:04:43 +0300

    wip

--- PhantomReferenceExample.java
+++ PhantomReferenceExample.java
@@ -1,42 +1,58 @@
 import org.junit.Assert;
 
 import java.lang.ref.PhantomReference;
-import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
-import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class PhantomReferenceExample {
 
     static class CustomObject {
-        final int externalResourceId = new Random().nextInt(); // any resource descriptor, etc
+        final long externalResourceId = System.currentTimeMillis(); // any resource descriptor, etc
     }
 
-    static class CustomFinalizer extends PhantomReference<CustomObject> {
-        private final int externalResourceId;
+    static class CustomObjectCleanup extends PhantomReference<CustomObject> {
+        private final long externalResourceId;
 
-        public CustomFinalizer(CustomObject referent, ReferenceQueue<CustomObject> q) {
+        CustomObjectCleanup(CustomObject referent, ReferenceQueue<CustomObject> q) {
             super(referent, q);
             // XXX do not store `referent` reference to this!
             this.externalResourceId = referent.externalResourceId;
         }
 
-        public void finalizeResources() {
-            System.out.println("PASSED: finalizeResources() invoked for " + externalResourceId);
+        void cleanupResources() {
+            System.out.println("PASSED: cleanupResources() invoked for " + externalResourceId);
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        ReferenceQueue que = new ReferenceQueue<CustomObject>();
-        CustomObject referent = new CustomObject();
-        CustomFinalizer phantom = new CustomFinalizer(referent, que);
-        referent = null; // GC-ready
+    private final ReferenceQueue<CustomObject> que = new ReferenceQueue<>();
+    private final Set<CustomObjectCleanup> phantoms = new HashSet<>();
 
-        System.gc();
+    private void markForCleanup(CustomObject referent) {
+        CustomObjectCleanup phantom = new CustomObjectCleanup(referent, que);
+        phantoms.add(phantom);
+    }
 
-        // you probably need a separate thread for real application
-        CustomFinalizer aRef = (CustomFinalizer)que.remove(); // XXX it's blocking
+    private void cleanupQueuedObjects() throws InterruptedException {
+        // you probably need to do it in a separate thread
+        System.out.println("awaiting queue");
+        CustomObjectCleanup aRef = (CustomObjectCleanup)que.remove(5_000); // XXX it's blocking
+        Assert.assertNotNull("the phantom reference never queued", aRef);
         Assert.assertNull(aRef.get());
-        aRef.finalizeResources();
+        aRef.cleanupResources();
         aRef.clear(); // not required since Java 9
+        phantoms.remove(aRef);
+        Assert.assertEquals(0, phantoms.size());
+    }
+
+    public static void main(String[] args) throws Exception {
+        PhantomReferenceExample example = new PhantomReferenceExample();
+        CustomObject referent = new CustomObject();
+        example.markForCleanup(referent);
+        referent = null; // GC ready
+        System.gc();
+        TimeUnit.MILLISECONDS.sleep(500);
+        example.cleanupQueuedObjects(); // use a dedicated thread
     }
 }
